174f250ace362b57307cf11eb0d905ccc48539bb
Brovko Danila <brd1200@yandex.ru>
2022-12-26 22:09:02 +1000

    removed the debug icon

--- lib/presentation/interface/app_in.dart
+++ lib/presentation/interface/app_in.dart
@@ -12,6 +12,7 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       //Заголовок и тема
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
